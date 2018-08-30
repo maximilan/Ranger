efa2be824aab9dd17b9ce0f1426f727b534af990
Clemens Herfarth <clemens@herfarth.com>
2018-08-30 15:48:04 +0200

    setup() moved to Utility

--- src/objects/Utility.java
+++ src/objects/Utility.java
@@ -5,4 +5,19 @@ public class Utility {
 	static int randint(double start, double end){
 		return (int) (start + Math.random() * (end-start));
 	}
+	
+	static Player setup(){
+	    Room r1 = new Room("A House", "is");
+	    r1.set_description("It is night. You find yourself standing in front of an abondoned villa.\nWindows and doors are boarded up. The only way out is a small path going south.");
+	    
+	    Room r2 = new Room("A Crossroads", "is");
+	    r2.set_description("You come to a crossroads. A disturbing silence greets you. \nYou seem to be alone.");
+	    
+	    r2.link("north", r1);
+	    r1.link("south", r2);
+	    
+	    
+	    Player player = new Player(r1);
+	    return player;
+	    }
 }
